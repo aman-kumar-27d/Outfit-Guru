c27a4a320ad41671a22b10c93a1f1d491fd37627
Aman Kumar <hijackhacker24by7@gmail.com>
2025-09-19 21:09:53 +0530

    W-1: All Dependency tested and ready to work with.

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,18 @@
 # FastAPI entrypoint
+
+from fastapi import FastAPI, UploadFile, File
+from PIL import Image
+import io
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Fashion Recommendation API is running"}
+
+@app.post("/upload/")
+async def upload_image(file: UploadFile = File(...)):
+    contents = await file.read()
+    img = Image.open(io.BytesIO(contents)).convert("RGB")
+    # TODO: call ML pipeline later
+    return {"filename": file.filename, "size": img.size}
